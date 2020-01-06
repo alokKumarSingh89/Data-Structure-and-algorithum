49b0334bff3ef8dc2ccce5f7a91149a0d6466d2f
Alok <itsaloksingh2011@gmail.com>
2020-01-06 06:57:08 +0000

    Rotate

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-console.log("Hello")
+const gcd = (a,b) =>{
+  if(b===0){
+    return a;
+  }
+  else return gcd(b,a%b);
+}
+const leftRotate = (arr,d) =>{
+  const length = arr.length;
+  d = d%length;
+  const _gcd = gcd(d,length); 
+  console.log(d,_gcd)
+}
+const arr = [1,2,3,4,5,6,7];
+d = 2;//rotation
+leftRotate(arr,d);
