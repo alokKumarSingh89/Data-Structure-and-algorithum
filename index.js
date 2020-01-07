e38aa362ce34d0bc8abff44e77ea0f9a522a34a0
Alok <itsaloksingh2011@gmail.com>
2020-01-07 02:11:48 +0000

    Done

--- index.js
+++ index.js
@@ -5,11 +5,28 @@ const gcd = (a,b) =>{
   else return gcd(b,a%b);
 }
 const leftRotate = (arr,d) =>{
-  const length = arr.length;
-  d = d%length;
-  const _gcd = gcd(d,length); 
-  console.log(d,_gcd)
+  const n = arr.length;
+  d = d%n;
+  const _gcd = gcd(d,n); 
+  for(let i=0;i<_gcd;i++){
+    let temp = arr[i];
+    let j = i;
+    labe2:while(true){
+      let k = j+d;
+      
+      if(k>=n){
+        k = k-n;
+      }
+      if(k==i){
+        break labe2;
+      }
+      arr[j] = arr[k];
+      j = k;
+    }
+    arr[j] = temp;
+  }
 }
 const arr = [1,2,3,4,5,6,7];
 d = 2;//rotation
 leftRotate(arr,d);
+console.log(arr);
